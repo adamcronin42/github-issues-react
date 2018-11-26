769f302c3d8bcf838e5a7b7097ecc3b810f95def
Adam <adamcronin42@gmail.com>
2018-11-26 13:34:54 -0600

    Adding environemtn variables and github graphql endpoint

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+require('dotenv').config();
 
+const cache = new InMemoryCache();
+const token = process.env.REACT_APP_GITHUB_TOKEN;
 const client = new ApolloClient({
-  url: 'https://api.github.com/graphql'
-})
+  link: createHttpLink({
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      authorization: `bearer ${token}`,
+    },
+  }),
+  cache,
+});
 
 render(
   <ApolloProvider client={client}>
