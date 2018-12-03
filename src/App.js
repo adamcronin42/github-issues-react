7e6e16dbd2a62d5e1745ea2419bcdf767eac2de1
Adam <adamcronin42@gmail.com>
2018-12-02 19:28:11 -0600

    Adding a key to the TableRow component

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
               <TableBody>
                 {issues.map(node => {
                   return (
-                    <TableRow>
+                    <TableRow key={node.number}>
                       <RowItem>{node.title}</RowItem>
                       <SecondaryRowItem>#{node.number} {util.getElapsedTime(new Date(node.createdAt))} by {node.author.login}</SecondaryRowItem>
                     </TableRow>
